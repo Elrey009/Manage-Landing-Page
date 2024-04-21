6fef1b2d5aeed092c185760c1dfca0f8c853bc8e
Elrey009 <daniloejike@gmail.com>
2024-04-21 17:41:36 +0100

    added nav list

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,21 @@
 import heroImg from "../assets/images/hero-img.png";
 const Hero = () => {
   return (
-    <section className=" font-BeVietnamPro text-[16px] flex flex-col gap-8 mt-10 ">
-      <div className=" px-6">
-        <img src={heroImg} className="w-[25rem] " />
+    <section className=" font-BeVietnamPro text-[16px] flex flex-col gap-8 mt-10 lg:flex-row-reverse lg:mx-[5rem] lg:py-6">
+      <div className=" px-6  md:mx-[12rem] lg:mx-0  lg:mr-20 ">
+        <img src={heroImg} className="w-[25rem] lg:mr-28 lg:w-[40rem]" />
       </div>
       <div>
-        <div className="flex flex-col gap-4 ">
-          <h2 className="font-bold text-4xl text-center text-DarkBlue">
+        <div className="flex flex-col gap-4 lg: ">
+          <h2 className="font-bold text-4xl text-center lg:ml-11 lg:text-left lg:pr-[12rem] text-DarkBlue">
             Bring everyone together to build better products.
           </h2>
-          <p className=" text-DarkGrayishBlue text-center px-[3rem] ">
+          <p className=" lg:pr-[12rem] lg:text-left text-DarkGrayishBlue text-center px-[3rem] ">
             Manage makes it simple for software teams to play day-day tasks
-            while keeping the team larger goals in view.
+            while keeping the larger team goals in view.
           </p>
         </div>
-        <div className=" w-36 bg-BrightRed h-12 mx-auto px-[1.6rem] py-[0.75rem] rounded-[23px] text-VeryPaleRed  my-4 shadow-lg lg:cusor-pointer whitespace-nowrap shadow-BrightRed mt-8">
+        <div className=" w-36 bg-BrightRed h-10 mx-auto px-[1.6rem] py-[0.55rem] rounded-[23px] text-VeryPaleRed  my-4 shadow-lg lg:cursor-pointer whitespace-nowrap shadow-BrightRed mt-8 lg:ml-11 lg:mt-12">
           Get Started
         </div>
       </div>
